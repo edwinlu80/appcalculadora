ed3e3474183c92cee865092fdf3320b2516a548d
edwinlu80 <elagunau@qbe.net.pe>
2022-03-12 22:42:58 -0500

    calculadora terminada

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -17,7 +17,42 @@ public class HomeController : Controller
     {
         return View();
     }
+    public IActionResult result(Operacion operacion)
+    {
+        ViewData["result"] = "La " + this.getType(operacion) + " es: "  + this.resolver(operacion);
+        return View("Index");
+    }
 
+    private double resolver(Operacion operacion)
+    {
+        switch (operacion.Type)
+        {
+            case 'S': 
+                return operacion.Number1 + operacion.Number2;
+            case 'R': 
+                return operacion.Number1 - operacion.Number2;
+            case 'M': 
+                return operacion.Number1 * operacion.Number2;
+            case 'D': 
+                return operacion.Number1 / operacion.Number2;
+
+            default: 
+                return 0.0;
+        }
+    }
+
+   private string getType(Operacion operacion)
+    {
+        switch (operacion.Type)
+        {
+            case 'S': return "suma";
+            case 'R': return "resta";
+            case 'M': return "multiplicacion";
+            case 'D': return "division";
+            default:  return "=(";
+        }
+    }
+ 
     public IActionResult Privacy()
     {
         return View();
---
+++ Models/Operacion.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace appcalculadora.Models
+{
+    public class Operacion
+    {
+        public double Number1 {get;set;}
+        public double Number2 {get;set;}
+
+        public char Type {get;set;}
+    }
+}
